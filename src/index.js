bbe6440516433392163309d74afaa0fd6c5a6d45
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2020-11-10 20:43:55 +0200

    Добавляє ДЗ

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./css/common.css";
 import ImagesApiService from "./js/imgages-api-service";
 import imageCard from "./templates/image-card-tpl.hbs";
+import animateScrollTo from "animated-scroll-to";
 
 const API_KEY = "19034439-608cc2a9f2617ed99a498289b";
 let searchQuery = "";
@@ -20,65 +21,133 @@ refs.loadMoreBtn.addEventListener("click", onLoadMore);
 
 function onSearch(e) {
   e.preventDefault();
+  clearImagesContainer();
+
   imagesApiService.query = e.currentTarget.elements.query.value;
   imagesApiService.resetPage();
   imagesApiService.fetchImages().then(appendImagesMarkup);
+
+  if (imagesApiService.query !== "") {
+    refs.loadMoreBtn.classList.remove("is-hidden");
+  }
 }
 
 function onLoadMore() {
   imagesApiService.fetchImages().then(appendImagesMarkup);
+  scrolling();
 
-  window.scrollBy({
-    top: document.documentElement.clientHeight - 100,
-    behavior: "smooth",
-  });
+  // const response = await imagesApiService.fetchImages();
+  // console.log("response", response);
+  // appendImagesMarkup(response);
 
-  //   window.scrollTo({
-  //     top: refs.imagesContainer.offsetHeight,
-  //     left: 0,
-  //     behavior: "smooth",
-  //   });
+  // console.log("resolve", resolve);
+  // imagesApiService.fetchImages().then(appendImagesMarkup);
+  // scrollToElement();
 
-  //   const height = document.documentElement.clientHeight;
+  // scroll2();
+  // scroll3();
 }
 
-function scroll() {
-  const scrollHeight = document.documentElement.scrollHeight;
-  console.log("!!!scrollHeight", scrollHeight);
+//   window.scrollTo({
+//     top: refs.imagesContainer.offsetHeight,
+//     left: 0,
+//     behavior: "smooth",
+//   });
 
-  const clientHeight = document.documentElement.clientHeight;
-  console.log("!!!clientHeight", clientHeight);
-  const delta = scrollHeight - clientHeight - clientHeight;
-  console.log("!!!delta", delta);
+//   const height = document.documentElement.clientHeight;
 
-  const scrollTop = document.documentElement.scrollTop;
-  console.log("!!!scrollTop", scrollTop);
-  //   const br = window.getBoundingClientRect();
-  //   console.log(br);
-  //   const top = br.top;
-  //   console.log(top);
+// function scroll() {
+//   const scrollHeight = document.documentElement.scrollHeight;
+//   console.log("!!!scrollHeight", scrollHeight);
 
-  window.scrollTo(0, Math.round(delta));
+//   const clientHeight = document.documentElement.clientHeight;
+//   console.log("!!!clientHeight", clientHeight);
+//   const delta = scrollHeight - clientHeight - clientHeight;
+//   console.log("!!!delta", delta);
 
-  //   alert(
-  //     "Top:" +
-  //       br.top +
-  //       ", Left:" +
-  //       br.left +
-  //       ", Right:" +
-  //       br.right +
-  //       ", Bottom:" +
-  //       br.bottom
-  //   );
+//   const scrollTop = document.documentElement.scrollTop;
+//   console.log("!!!scrollTop", scrollTop);
+//   //   const br = window.getBoundingClientRect();
+//   //   console.log(br);
+//   //   const top = br.top;
+//   //   console.log(top);
 
-  //   const height = document.documentElement.clientHeight;
-  // console.log("height", height);
-  // console.log("!!!ВИСОТА", window.pageYOffset);
+//   // window.scrollTo(0, Math.round(delta));
 
-  // const height = document.documentElement.clientHeight;
-  // console.log("!!!height", height);
-}
+//   //   alert(
+//   //     "Top:" +
+//   //       br.top +
+//   //       ", Left:" +
+//   //       br.left +
+//   //       ", Right:" +
+//   //       br.right +
+//   //       ", Bottom:" +
+//   //       br.bottom
+//   //   );
+
+//   //   const height = document.documentElement.clientHeight;
+//   // console.log("height", height);
+//   // console.log("!!!ВИСОТА", window.pageYOffset);
+
+//   // const height = document.documentElement.clientHeight;
+//   // console.log("!!!height", height);
+// }
 
 function appendImagesMarkup(hits) {
   refs.imagesContainer.insertAdjacentHTML("beforeend", imageCard(hits));
+}
+
+function clearImagesContainer() {
+  refs.imagesContainer.innerHTML = "";
+}
+
+// function scrollToElement() {
+//   // const options = {
+//   //   speed: 500,
+//   //   verticalOffset: -10,
+//   // };
+
+//   const scrollHeight = document.documentElement.scrollHeight;
+//   // // console.log("!!!scrollHeight", scrollHeight);
+
+//   const clientHeight = document.documentElement.clientHeight;
+//   // // console.log("!!!clientHeight", clientHeight);
+//   const delta = scrollHeight - clientHeight / 2;
+
+//   const scrollOptions = {
+//     left: 0,
+//     top: delta,
+//     behavior: "smooth",
+//   };
+
+//   // window.scrollTo(scrollOptions);
+//   // window.scrollBy(0, window.innerHeight);
+
+//   // animateScrollTo(delta, options);
+// }
+
+function scroll2() {
+  scrollY += window.innerHeight;
+  // console.log(scrollY);
+  window.scrollTo({
+    top: scrollY,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+function scrolling() {
+  const scrollHeight = document.documentElement.scrollHeight;
+
+  const clientHeight = document.documentElement.clientHeight;
+  const delta = scrollHeight - clientHeight;
+
+  const scrollOptions = {
+    top: delta + 100,
+    left: 0,
+    behavior: "smooth",
+  };
+  setTimeout(() => {
+    window.scrollTo(scrollOptions);
+  }, 500);
 }
