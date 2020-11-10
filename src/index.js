bc52fd29f56e0356c6f6860df1bc42bc2e6a32dc
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2020-11-11 00:14:07 +0200

    Добавляє ДЗ

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import "./css/common.css";
 import ImagesApiService from "./js/imgages-api-service";
 import imageCard from "./templates/image-card-tpl.hbs";
-import animateScrollTo from "animated-scroll-to";
 
-const API_KEY = "19034439-608cc2a9f2617ed99a498289b";
-let searchQuery = "";
+import "@pnotify/core/dist/PNotify.css";
+import { error } from "@pnotify/core";
+import "@pnotify/core/dist/BrightTheme.css";
+
+import * as basicLightbox from "basiclightbox";
+import "basiclightbox/dist/basicLightbox.min.css";
 
 const imagesApiService = new ImagesApiService();
 
@@ -16,6 +19,7 @@ const refs = {
 
 refs.searchForm.addEventListener("submit", onSearch);
 refs.loadMoreBtn.addEventListener("click", onLoadMore);
+refs.imagesContainer.addEventListener("click", onOpenModal);
 
 // console.log("!!!imagesApiService", imagesApiService);
 
@@ -26,72 +30,18 @@ function onSearch(e) {
   imagesApiService.query = e.currentTarget.elements.query.value;
   imagesApiService.resetPage();
   imagesApiService.fetchImages().then(appendImagesMarkup);
+  if (imagesApiService.query === "") {
+    onInfoNoTextForResearch();
+  }
 
   if (imagesApiService.query !== "") {
     refs.loadMoreBtn.classList.remove("is-hidden");
   }
 }
-
 function onLoadMore() {
   imagesApiService.fetchImages().then(appendImagesMarkup);
   scrolling();
-
-  // const response = await imagesApiService.fetchImages();
-  // console.log("response", response);
-  // appendImagesMarkup(response);
-
-  // console.log("resolve", resolve);
-  // imagesApiService.fetchImages().then(appendImagesMarkup);
-  // scrollToElement();
-
-  // scroll2();
-  // scroll3();
 }
-
-//   window.scrollTo({
-//     top: refs.imagesContainer.offsetHeight,
-//     left: 0,
-//     behavior: "smooth",
-//   });
-
-//   const height = document.documentElement.clientHeight;
-
-// function scroll() {
-//   const scrollHeight = document.documentElement.scrollHeight;
-//   console.log("!!!scrollHeight", scrollHeight);
-
-//   const clientHeight = document.documentElement.clientHeight;
-//   console.log("!!!clientHeight", clientHeight);
-//   const delta = scrollHeight - clientHeight - clientHeight;
-//   console.log("!!!delta", delta);
-
-//   const scrollTop = document.documentElement.scrollTop;
-//   console.log("!!!scrollTop", scrollTop);
-//   //   const br = window.getBoundingClientRect();
-//   //   console.log(br);
-//   //   const top = br.top;
-//   //   console.log(top);
-
-//   // window.scrollTo(0, Math.round(delta));
-
-//   //   alert(
-//   //     "Top:" +
-//   //       br.top +
-//   //       ", Left:" +
-//   //       br.left +
-//   //       ", Right:" +
-//   //       br.right +
-//   //       ", Bottom:" +
-//   //       br.bottom
-//   //   );
-
-//   //   const height = document.documentElement.clientHeight;
-//   // console.log("height", height);
-//   // console.log("!!!ВИСОТА", window.pageYOffset);
-
-//   // const height = document.documentElement.clientHeight;
-//   // console.log("!!!height", height);
-// }
 
 function appendImagesMarkup(hits) {
   refs.imagesContainer.insertAdjacentHTML("beforeend", imageCard(hits));
@@ -99,41 +49,6 @@ function appendImagesMarkup(hits) {
 
 function clearImagesContainer() {
   refs.imagesContainer.innerHTML = "";
-}
-
-// function scrollToElement() {
-//   // const options = {
-//   //   speed: 500,
-//   //   verticalOffset: -10,
-//   // };
-
-//   const scrollHeight = document.documentElement.scrollHeight;
-//   // // console.log("!!!scrollHeight", scrollHeight);
-
-//   const clientHeight = document.documentElement.clientHeight;
-//   // // console.log("!!!clientHeight", clientHeight);
-//   const delta = scrollHeight - clientHeight / 2;
-
-//   const scrollOptions = {
-//     left: 0,
-//     top: delta,
-//     behavior: "smooth",
-//   };
-
-//   // window.scrollTo(scrollOptions);
-//   // window.scrollBy(0, window.innerHeight);
-
-//   // animateScrollTo(delta, options);
-// }
-
-function scroll2() {
-  scrollY += window.innerHeight;
-  // console.log(scrollY);
-  window.scrollTo({
-    top: scrollY,
-    left: 0,
-    behavior: "smooth",
-  });
 }
 
 function scrolling() {
@@ -150,4 +65,17 @@ function scrolling() {
   setTimeout(() => {
     window.scrollTo(scrollOptions);
   }, 500);
+}
+
+function onInfoNoTextForResearch() {
+  error({
+    text: "No search query. Enter text to search.",
+    delay: 3000,
+  });
+}
+
+function onOpenModal(e) {
+  const instance = basicLightbox.create(`
+    <img src=${e.target.dataset.src}>`);
+  instance.show();
 }
